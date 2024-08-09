414ee3cdccb666219da196cf03e87058f7ea72c5
Erick Noreña <sanoci05@gmail.com>
2024-08-09 15:44:04 -0500

    delete

--- BibliotecaX.sql
+++ BibliotecaX.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-08-2024 a las 16:41:16
+-- Tiempo de generación: 08-08-2024 a las 16:53:15
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `practica1`
+-- Base de datos: `bibliotecax`
 --
 
 DELIMITER $$
@@ -133,6 +133,36 @@ INSERT INTO `audi_autor` (`aut_codigo_audi`, `aut_apellido_old`, `aut_nacimiento
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `audi_libro`
+--
+
+CREATE TABLE `audi_libro` (
+  `lib_isbn_audi` int(11) NOT NULL,
+  `num_audi_libro` int(11) DEFAULT NULL,
+  `lib_titulo_old` varchar(255) DEFAULT NULL,
+  `lib_genero_old` varchar(20) DEFAULT NULL,
+  `lib_numeroPaginas_old` int(11) DEFAULT NULL,
+  `lib_dias_old` tinyint(4) DEFAULT NULL,
+  `lib_titulo_new` varchar(255) DEFAULT NULL,
+  `lib_genero_new` varchar(20) DEFAULT NULL,
+  `lib_numeroPaginas_new` int(11) DEFAULT NULL,
+  `lib_dias_new` tinyint(4) DEFAULT NULL,
+  `lib_fechaModificacion` datetime DEFAULT NULL,
+  `lib_usuario` varchar(10) DEFAULT NULL,
+  `lib_accion` varchar(55) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `audi_libro`
+--
+
+INSERT INTO `audi_libro` (`lib_isbn_audi`, `num_audi_libro`, `lib_titulo_old`, `lib_genero_old`, `lib_numeroPaginas_old`, `lib_dias_old`, `lib_titulo_new`, `lib_genero_new`, `lib_numeroPaginas_new`, `lib_dias_new`, `lib_fechaModificacion`, `lib_usuario`, `lib_accion`) VALUES
+(1, 2147483647, 'El Enigma de los Espejos Rotos', 'romance', 156, 7, 'Los hmnos Grimm', 'romance', 156, 7, '2024-08-08 09:30:50', 'root@local', 'UPDATE'),
+(3, 202020, 'Los 3 Cerditos', 'Infantil', 10, 3, NULL, NULL, NULL, NULL, '2024-08-08 09:36:50', 'root@local', 'DELETE');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `audi_socio`
 --
 
@@ -151,6 +181,16 @@ CREATE TABLE `audi_socio` (
   `audi_usuario` varchar(10) DEFAULT NULL,
   `audi_accion` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `nombre_libros`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `nombre_libros` (
+`lib_titulo` varchar(255)
+);
 
 -- --------------------------------------------------------
 
@@ -267,7 +307,32 @@ INSERT INTO `tbl_libro` (`lib_isbn`, `lib_titulo`, `lib_genero`, `lib_numeroPagi
 (8888888888, 'La Ciudad de los Susurros', 'Misterio', 274, 1),
 (9517530862, 'Las Crónicas del Eco Silencioso', 'fantasía', 448, 7),
 (9876543210, 'El Laberinto de los Recuerdos', 'cuento', 412, 7),
-(9999999999, 'El Enigma de los Espejos Rotos', 'romance', 156, 7);
+(9999999999, 'Los hmnos Grimm', 'romance', 156, 7);
+
+--
+-- Disparadores `tbl_libro`
+--
+DELIMITER $$
+CREATE TRIGGER `libro_delete` BEFORE DELETE ON `tbl_libro` FOR EACH ROW BEGIN
+	INSERT INTO audi_libro (num_audi_libro, lib_titulo_old, lib_genero_old,
+	lib_numeroPaginas_old, lib_dias_old, lib_fechaModificacion, lib_usuario, lib_accion)
+    VALUES (old.lib_isbn, old.lib_titulo, old.lib_genero, old.lib_numeroPaginas,
+    old.lib_diasPrestamo, NOW(), USER(), 'DELETE');
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `libro_update` BEFORE UPDATE ON `tbl_libro` FOR EACH ROW BEGIN
+	INSERT INTO audi_libro (num_audi_libro,lib_titulo_old,lib_genero_old,
+	lib_numeroPaginas_old,lib_dias_old,lib_titulo_new,lib_genero_new,
+	lib_numeroPaginas_new,lib_dias_new,lib_fechaModificacion,lib_usuario,
+	lib_accion)
+    VALUES (old.lib_isbn, old.lib_titulo, old.lib_genero, old.lib_numeroPaginas,
+    old.lib_diasPrestamo, new.lib_titulo, new.lib_genero, new.lib_numeroPaginas, 
+    new.lib_diasPrestamo, NOW(), USER(), 'UPDATE');
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -288,15 +353,10 @@ CREATE TABLE `tbl_prestamo` (
 --
 
 INSERT INTO `tbl_prestamo` (`pres_id`, `pres_fechaPrestamo`, `pres_fechaDevolucion`, `soc_copiaNumero`, `lib_copiaisbn`) VALUES
-('pres1', '2023-01-15', '2023-01-20', 1, 1234567890),
-('pres2', '2023-02-03', '2023-02-04', 2, 9999999999),
-('pres3', '2023-04-09', '2023-04-11', 6, 2718281828),
-('pres4', '2023-06-14', '2023-06-15', 9, 8888888888),
-('pres5', '2023-07-02', '2023-07-09', 10, 5555555555),
 ('pres6', '2023-08-19', '2023-08-26', 12, 5555555555),
-('pres7', '2023-10-24', '2023-10-27', 3, 1357924680),
+('pres7', '2023-10-24', '2024-08-27', 3, 1357924680),
 ('pres8', '2023-11-11', '2023-11-12', 4, 9999999999),
-('pres9', '2023-12-29', '2023-12-30', 8, 5555555555);
+('pres9', '2023-12-29', '2024-08-30', 8, 5555555555);
 
 -- --------------------------------------------------------
 
@@ -417,6 +477,34 @@ INSERT INTO `tbl_tipoautores` (`copiaisbn`, `copiaAutor`, `tipoAutor`) VALUES
 (7777777777, 765, 'Autor'),
 (9999999999, 98, 'Autor');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vista_autor`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vista_autor` (
+`aut_apellido` varchar(45)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `nombre_libros`
+--
+DROP TABLE IF EXISTS `nombre_libros`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `nombre_libros`  AS SELECT `tbl_libro`.`lib_titulo` AS `lib_titulo` FROM `tbl_libro` ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vista_autor`
+--
+DROP TABLE IF EXISTS `vista_autor`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_autor`  AS SELECT `tbl_autor`.`aut_apellido` AS `aut_apellido` FROM `tbl_autor` ;
+
 --
 -- Índices para tablas volcadas
 --
@@ -426,6 +514,12 @@ INSERT INTO `tbl_tipoautores` (`copiaisbn`, `copiaAutor`, `tipoAutor`) VALUES
 --
 ALTER TABLE `audi_autor`
   ADD PRIMARY KEY (`aut_codigo_audi`);
+
+--
+-- Indices de la tabla `audi_libro`
+--
+ALTER TABLE `audi_libro`
+  ADD PRIMARY KEY (`lib_isbn_audi`);
 
 --
 -- Indices de la tabla `audi_socio`
@@ -443,7 +537,8 @@ ALTER TABLE `tbl_autor`
 -- Indices de la tabla `tbl_libro`
 --
 ALTER TABLE `tbl_libro`
-  ADD PRIMARY KEY (`lib_isbn`);
+  ADD PRIMARY KEY (`lib_isbn`),
+  ADD KEY `idx_titulo` (`lib_titulo`);
 
 --
 -- Indices de la tabla `tbl_prestamo`
@@ -477,6 +572,12 @@ ALTER TABLE `audi_autor`
   MODIFY `aut_codigo_audi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT de la tabla `audi_libro`
+--
+ALTER TABLE `audi_libro`
+  MODIFY `lib_isbn_audi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `audi_socio`
 --
 ALTER TABLE `audi_socio`
@@ -499,6 +600,17 @@ ALTER TABLE `tbl_prestamo`
 ALTER TABLE `tbl_tipoautores`
   ADD CONSTRAINT `tbl_tipoautores_ibfk_1` FOREIGN KEY (`copiaisbn`) REFERENCES `tbl_libro` (`lib_isbn`),
   ADD CONSTRAINT `tbl_tipoautores_ibfk_2` FOREIGN KEY (`copiaAutor`) REFERENCES `tbl_autor` (`aut_codigo`);
+
+DELIMITER $$
+--
+-- Eventos
+--
+CREATE DEFINER=`root`@`localhost` EVENT `minuto_Aliminar_prestamos` ON SCHEDULE EVERY 1 MINUTE STARTS '2024-08-08 06:52:29' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN 
+	DELETE FROM tbl_prestamo
+    WHERE pres_fechaPrestamo <= NOW()  - INTERVAL 1 YEAR;
+END$$
+
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
